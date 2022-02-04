e8dcbe0a9a62a6080eb190bde67c745be32d38fa
jashoney <4479@holbertonschool.com>
2022-02-04 11:03:09 +1100

    added task 7

--- 0x01-variables_if_else_while/7-print_tebahpla.c
+++ 0x01-variables_if_else_while/7-print_tebahpla.c
@@ -1,6 +1,17 @@
 #include <stdio.h>
 
-int main()
+/**
+ * main - prints the lower case alphabet in reverse and a new line
+ * @c - character that will hold each letter
+ *
+ * Return: the function main returns 0
+ */
+int main(void)
 {
+	char c;
+
+	for (c = 'z'; c >= 'a'; --c)
+		putchar(c);
+	putchar('\n');
 	return (0);
 }
