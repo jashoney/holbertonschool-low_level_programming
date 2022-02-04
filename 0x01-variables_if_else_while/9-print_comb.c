d81088399d7348459544b1e96bd38bc08248d379
jashoney <4479@holbertonschool.com>
2022-02-04 13:47:54 +1100

    task 9 added

--- 0x01-variables_if_else_while/9-print_comb.c
+++ 0x01-variables_if_else_while/9-print_comb.c
@@ -1,6 +1,26 @@
 #include <stdio.h>
 
-int main()
+/**
+ * main - the main function short description
+ *
+ * Return: returns zero
+ */
+int main(void)
 {
+	char numarray[20] = {'0', '1', '2', '3', '4', '5', '6', '7', '8', '9'};
+	int i;
+
+	i = 0;
+	while (i < 10)
+	{
+		putchar(numarray[i]);
+		if (i < 9)
+		{
+			putchar(44);
+			putchar(32);
+		}
+		i++;
+	}
+	putchar('\n');
 	return (0);
 }
