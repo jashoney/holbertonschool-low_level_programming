cdad9e989dc65146e053e800ec74df43f70f2464
jashoney <4479@holbertonschool.com>
2022-02-04 12:59:32 +1100

    added task 8

--- 0x01-variables_if_else_while/8-print_base16.c
+++ 0x01-variables_if_else_while/8-print_base16.c
@@ -1,6 +1,17 @@
 #include <stdio.h>
 
-int main()
+/**
+ * main - prints the numbers 0 up to 9 and then a new line
+ * @number - integer that will hold each number
+ *
+ * Return: the function main returns 0
+ */
+int main(void)
 {
+	int number;
+
+	for (number = '0'; number <= '9'; ++number)
+		putchar(number);
+	putchar('\n');
 	return (0);
 }
