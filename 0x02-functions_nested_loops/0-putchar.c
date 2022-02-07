dac6550bab8e75b91e6228b7915388195a08fc07
jashoney <4479@holbertonschool.com>
2022-02-08 10:45:17 +1100

    added task 0

--- 0x02-functions_nested_loops/0-putchar.c
+++ 0x02-functions_nested_loops/0-putchar.c
@@ -1,4 +1,4 @@
-#include <stdio.h>
+#include "main.h"
 
 /**
  * main - check the code
@@ -7,8 +7,13 @@
  */
 int main(void)
 {
-	printf("_putchar\n");
+	char sentence[9] = "_putchar\n";
+	int count = 0;
+
+	while (count <= 8)
+	{
+		_putchar(sentence[count]);
+		count++;
+	}
 	return (0);
 }
-
-
