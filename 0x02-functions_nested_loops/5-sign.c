d2634bef74f1d340c275c51b298a7d7e5f276455
jashoney <4479@holbertonschool.com>
2022-02-08 13:02:32 +1100

    added task 5

---
+++ 0x02-functions_nested_loops/5-sign.c
@@ -0,0 +1,25 @@
+#include "main.h"
+
+/**
+ * print_sign - returns 1 if n is positive, 0 if zero, or -1 if negative
+ * n: the number of input
+ * Return: as above
+ */
+int print_sign(int n)
+{
+	if (n > 0) 
+	{	
+		_putchar('+');
+		return (1);
+	}
+	if (n == 0)
+	{
+		_putchar('0');
+		return (0);
+	}
+	else 
+	{		
+		_putchar('-');
+		return (-1);
+	}
+}
