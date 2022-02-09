70ea5a629a230b260a86090648c2b4edcd350631
jashoney <4479@holbertonschool.com>
2022-02-09 15:23:51 +1100

    changed task 3

--- 0x03-debugging/3-print_remaining_days.c
+++ 0x03-debugging/3-print_remaining_days.c
@@ -2,14 +2,13 @@
 #include "main.h"
 
 /**
- * print_remaining_days - takes a date and prints how many days are left in a year
- * 	
+ * print_remaining_days - prints how many days are left in a year
+ *
  * @month: month input
  * @day: day of month
  * @year: year input
  * Return: is void
  */
-
 void print_remaining_days(int month, int day, int year)
 {
 	if (year % 4 == 0)
@@ -25,6 +24,6 @@ void print_remaining_days(int month, int day, int year)
 	else
 	{
 		printf("Day of the year: %d\n", day);
-		printf("Remaining days: %d\n", 365 - day);	
+		printf("Remaining days: %d\n", 365 - day);
 	}
 }
