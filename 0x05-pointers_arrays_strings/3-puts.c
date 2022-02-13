453f31fcba7bd6e55382b9957735c7a2f296d9a6
jashoney <4479@holbertonschool.com>
2022-02-14 09:53:34 +1100

    added task 3

---
+++ 0x05-pointers_arrays_strings/3-puts.c
@@ -0,0 +1,22 @@
+#include "main.h"
+
+/**
+ * _puts - prints a string
+ * str: is a pointer to an string of characters
+ * Return: void
+ */
+void _puts(char *str)
+{
+	char stringchar;
+	int stringlength = 0;
+
+	stringchar = *str;
+	while (stringchar != 0)
+	{
+		_putchar(stringchar);
+		stringlength++;
+		stringchar = *(str + stringlength);
+	}
+	_putchar('\n');
+}
+
