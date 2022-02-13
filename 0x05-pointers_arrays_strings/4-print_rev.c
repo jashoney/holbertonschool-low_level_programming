4f8a2b85ed21647a1d7a9a9189335bf6c99bf209
jashoney <4479@holbertonschool.com>
2022-02-14 10:19:03 +1100

    added task 4

---
+++ 0x05-pointers_arrays_strings/4-print_rev.c
@@ -0,0 +1,28 @@
+#include "main.h"
+
+/**
+ * print_rev - prints a string in reverse
+ * s: is a pointer to an string of characters
+ * Return: void
+ */
+void print_rev(char *s)
+{
+	char stringchar;
+	int stringlength = 0, reverse;
+
+	stringchar = *s;
+	while (stringchar != 0)
+	{
+		stringlength++;
+		stringchar = *(s + stringlength);
+	}
+	reverse = stringlength - 1;
+	while (reverse >= 0)
+		{
+			stringchar = *(s + reverse);
+			_putchar(stringchar);
+			reverse--;
+		}
+	_putchar('\n');
+}
+
