b08605ad704d2c616f9c9d230b378719a5bcd594
jashoney <4479@holbertonschool.com>
2022-02-15 08:33:59 +1100

    added task 100

---
+++ 0x05-pointers_arrays_strings/100-atoi.c
@@ -0,0 +1,68 @@
+#include "main.h"
+#include <stdio.h>
+
+/**
+ * strlength - finds the length of a string
+ * @src: is a pointer to an string of characters
+ * Return: void
+ */
+int strlength(char *src)
+{
+	int length = 0;
+
+	while (src[length] != '\0')
+		length++;
+	return (length);
+}
+
+/**
+ * strcut - cuts the integers out of a string
+ * @src: points to the string
+ * @length: length of the string chars
+ * Return: the integer that is left, which must be signed
+ */
+
+int strcut(char *src, int length)
+{
+	int number = 0, signcount = 0, loop = 0;
+
+	while (loop <= length)
+	{
+		if (src[loop] >= '0' && src[loop] <= '9')
+		{
+			number = number * 10 + (src[loop] - '0');
+			printf("number is currently : %d\n", number);
+		}
+		else if (src[loop] == '-' || src[loop] == '+')
+		{
+			if (src[loop] == '-')
+				signcount--;
+			else
+				signcount++;
+		}
+		else
+		{
+			if (number > 0)
+				break;
+		}
+		loop++;
+	}
+	if (signcount < 0)
+		number = number * -1;
+	return (number);
+}
+/**
+ * _atoi - input string at *src and convert it to integer
+ * @src: points to a string
+ * Return: the pointer dest
+ */
+int _atoi(char *src)
+{
+	int stringlength = 0, newint = 0;
+
+	stringlength = strlength(src);
+	printf("stringlength is: %d\n", stringlength);
+	if (stringlength > 0)
+		newint = strcut(src, stringlength);
+	return (newint);
+}
