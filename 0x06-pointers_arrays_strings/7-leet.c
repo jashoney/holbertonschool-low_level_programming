53e0d204e852d98d1cea2c3b66fc4238db47b7d0
jashoney <4479@holbertonschool.com>
2022-02-17 08:52:41 +1100

    fixed loop

--- 0x06-pointers_arrays_strings/7-leet.c
+++ 0x06-pointers_arrays_strings/7-leet.c
@@ -13,7 +13,7 @@ void is_changechar(char *s)
 	int candidates[10] = {97, 65, 101, 69, 111, 79, 116, 84, 108, 76};
 	int replacements[10] = {52, 52, 51, 51, 48, 48, 55, 55, 49, 49};
 
-	while (i < 13)
+	while (i < 10)
 	{
 		if (*s == candidates[i])
 			*s = replacements[i];
