18c96fc0719479718da95f628e55cda56d716e3f
jashoney <4479@holbertonschool.com>
2022-02-16 14:10:32 +1100

    added task 4

--- 0x06-pointers_arrays_strings/4-rev_array.c
+++ 0x06-pointers_arrays_strings/4-rev_array.c
@@ -9,19 +9,18 @@
 void reverse_array(int *a, int n)
 {
 	int i = 0, temp;
-/* 
- * This function takes a ptr to an array of numbers 
+/*
+ * This function takes a ptr to an array of numbers
  * of length n, and reverses the content of the array
- * it does this by pointing to the start and to the end 
+ * it does this by pointing to the start and to the end
  * numbers, swapping them, and then repeating this process
  * as the pointers converge toward the middle of the array
  */
 	while (i < n / 2)
 	{
 		temp = a[n - 1 - i];
-		a[n -1 - i] = a[i];
+		a[n - 1 - i] = a[i];
 		a[i] = temp;
-		i++;	
+		i++;
 	}
-	
 }
