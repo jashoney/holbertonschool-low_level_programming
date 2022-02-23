37065193d69196f1a519abfc985dec5ecf1531f6
jashoney <4479@holbertonschool.com>
2022-02-23 12:38:29 +1100

    changed task 5 to cater of 1 and 0

--- 0x08-recursion/5-sqrt_recursion.c
+++ 0x08-recursion/5-sqrt_recursion.c
@@ -24,6 +24,14 @@ int _sqrt_recursion(int n)
 {
 	if (n < 0)
 		return (-1);
-	else
+
+	if (n == 0)
+		return (0);
+
+	if (n == 1)
+		return (1);
+
+	if (n > 2)
 		return (test_function(1, n));
+	return (-1);
 }
