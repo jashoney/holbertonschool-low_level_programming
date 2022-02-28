e3a8242d3933440a2800b6a4b81b2a3d301f1d90
jashoney <4479@holbertonschool.com>
2022-03-01 07:11:55 +1100

    added task 1

---
+++ 0x0B-malloc_free/1-strdup.c
@@ -0,0 +1,32 @@
+#include "main.h"
+#include <stdlib.h>
+
+/**
+ * _strdup - copies a string
+ * @str: pointer to the string
+ * Return: dest or NULL
+ */
+char *_strdup(char *str)
+{
+	char *dest;
+	int i = 0, size = 0;
+
+	if (str == NULL)
+		return (NULL);
+
+	while (str[size] != '\0')
+		size++;
+
+	dest = malloc(size * sizeof(str[0]));
+
+	if (dest == NULL)
+		return (NULL);
+
+	while (i < size)
+	{
+		dest[i] = str[i];
+		i++;
+	}
+
+	return (dest);
+}
