a89300cdea1b6edf7e378766a252d71fa2179dea
jashoney <4479@holbertonschool.com>
2022-03-01 16:41:51 +1100

    added task 100

---
+++ 0x0B-malloc_free/100-argstostr.c
@@ -0,0 +1,71 @@
+#include "main.h"
+#include <stdlib.h>
+#include <stdio.h>
+/**
+ * str_concat - cats two strings together
+ * @s1: ptr to the first string
+ * @s2: ptr to the second string
+ * Return: dest or NULL
+ */
+char *str_concat(char *s1, char *s2)
+{
+        int i, s1size = 0, s2size = 0;
+        char charsize, *dest;
+
+        if (s1 == NULL && s2 == NULL)
+        {
+                dest = malloc(1 * sizeof(charsize));
+                dest[0] = '\0';
+                return (dest);
+        }
+        if (s1 != NULL)
+        {
+                while (s1[s1size] != '\0')
+                        s1size++;
+        }
+        if (s2 != NULL)
+        {
+                while (s2[s2size] != '\0')
+                        s2size++;
+        }
+        if (s1size > 0)
+                dest = malloc(1 + (s1size + s2size) * sizeof(s1[0]));
+        else
+                dest = malloc(1 + (s1size + s2size) * sizeof(s2[0]));
+        if (dest == NULL)
+                return (NULL);
+        for (i = 0; i < s1size; i++)
+                dest[i] = s1[i];
+        if (s2size == 0)
+                dest[s1size + 1] = '\0';
+        else
+        {
+                for (i = 0; i <= s2size; i++)
+                        dest[i + s1size] = s2[i];
+        }
+        return (dest);
+}
+
+/*
+ * argstostr - concatonates all the arguments of a program
+ * ac: is argc:
+ * av: is argv: a ptr to a ptr of chars
+ * Return: a ptr to a new string or NULL
+ */
+char *argstostr(int ac, char **av)
+{
+	int i = 1;
+	char *newstring = NULL;
+
+	if (ac == 0) 
+		return (NULL);
+	if (av == NULL)
+		return (NULL);
+	while (i < ac)
+	{
+		newstring = str_concat(newstring, av[i]);
+		newstring = str_concat(newstring, "\n");
+		i++;
+	}
+	return (newstring);
+}
