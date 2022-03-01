e7e506f65cbeea19ec0971b97472f91e5d828631
jashoney <4479@holbertonschool.com>
2022-03-01 16:47:12 +1100

    betty applied to task 100

--- 0x0B-malloc_free/100-argstostr.c
+++ 0x0B-malloc_free/100-argstostr.c
@@ -9,47 +9,47 @@
  */
 char *str_concat(char *s1, char *s2)
 {
-        int i, s1size = 0, s2size = 0;
-        char charsize, *dest;
+	int i, s1size = 0, s2size = 0;
+	char charsize, *dest;
 
-        if (s1 == NULL && s2 == NULL)
-        {
-                dest = malloc(1 * sizeof(charsize));
-                dest[0] = '\0';
-                return (dest);
-        }
-        if (s1 != NULL)
-        {
-                while (s1[s1size] != '\0')
-                        s1size++;
-        }
-        if (s2 != NULL)
-        {
-                while (s2[s2size] != '\0')
-                        s2size++;
-        }
-        if (s1size > 0)
-                dest = malloc(1 + (s1size + s2size) * sizeof(s1[0]));
-        else
-                dest = malloc(1 + (s1size + s2size) * sizeof(s2[0]));
-        if (dest == NULL)
-                return (NULL);
-        for (i = 0; i < s1size; i++)
-                dest[i] = s1[i];
-        if (s2size == 0)
-                dest[s1size + 1] = '\0';
-        else
-        {
-                for (i = 0; i <= s2size; i++)
-                        dest[i + s1size] = s2[i];
-        }
-        return (dest);
+	if (s1 == NULL && s2 == NULL)
+	{
+		dest = malloc(1 * sizeof(charsize));
+		dest[0] = '\0';
+		return (dest);
+	}
+	if (s1 != NULL)
+	{
+		while (s1[s1size] != '\0')
+			s1size++;
+	}
+	if (s2 != NULL)
+	{
+		while (s2[s2size] != '\0')
+			s2size++;
+	}
+	if (s1size > 0)
+		dest = malloc(1 + (s1size + s2size) * sizeof(s1[0]));
+	else
+		dest = malloc(1 + (s1size + s2size) * sizeof(s2[0]));
+	if (dest == NULL)
+		return (NULL);
+	for (i = 0; i < s1size; i++)
+		dest[i] = s1[i];
+	if (s2size == 0)
+		dest[s1size + 1] = '\0';
+	else
+	{
+		for (i = 0; i <= s2size; i++)
+			dest[i + s1size] = s2[i];
+	}
+	return (dest);
 }
 
-/*
+/**
  * argstostr - concatonates all the arguments of a program
- * ac: is argc:
- * av: is argv: a ptr to a ptr of chars
+ * @ac: is argc
+ * @av: is argv: a ptr to a ptr of chars
  * Return: a ptr to a new string or NULL
  */
 char *argstostr(int ac, char **av)
@@ -57,7 +57,7 @@ char *argstostr(int ac, char **av)
 	int i = 1;
 	char *newstring = NULL;
 
-	if (ac == 0) 
+	if (ac == 0)
 		return (NULL);
 	if (av == NULL)
 		return (NULL);
