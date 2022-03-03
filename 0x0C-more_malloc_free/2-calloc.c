a1311e4cb312d29a3bb236465a904791c1d0aaea
jashoney <4479@holbertonschool.com>
2022-03-03 12:32:27 +1100

    betty task 2

--- 0x0C-more_malloc_free/2-calloc.c
+++ 0x0C-more_malloc_free/2-calloc.c
@@ -9,12 +9,21 @@
  */
 void *_calloc(unsigned int nmemb, unsigned int size)
 {
-	void *ptr;
+	char *ptr;
+	unsigned int i = 0;
 
 	if (nmemb == 0 || size == 0)
 		return (NULL);
+
 	ptr = malloc(size * nmemb);
 	if (ptr == NULL)
 		return (NULL);
+
+	while (i < nmemb)
+	{
+		ptr[i] = 0;
+		i++;
+	}
+
 	return (ptr);
 }
