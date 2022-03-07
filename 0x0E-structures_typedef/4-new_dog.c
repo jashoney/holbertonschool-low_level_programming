724ea0190ae6aa414141115f8967416c75093336
jashoney <4479@holbertonschool.com>
2022-03-07 13:33:54 +1100

    added _strdup function to make string copy

--- 0x0E-structures_typedef/4-new_dog.c
+++ 0x0E-structures_typedef/4-new_dog.c
@@ -1,6 +1,36 @@
 #include "dog.h"
 #include <stdlib.h>
 #include <stdio.h>
+/**
+ * _strdup - copies a string
+ * @str: pointer to the string
+ * Return: dest or NULL
+ */
+
+char *_strdup(char *str)
+{
+        char *dest;
+        int i = 0, size = 0;
+
+        if (str == NULL)
+                return (NULL);
+
+        while (str[size] != '\0')
+                size++;
+
+        dest = malloc(1 + size * sizeof(str[0]));
+
+        if (dest == NULL)
+                return (NULL);
+
+        while (i <= size)
+        {
+                dest[i] = str[i];
+                i++;
+        }
+
+        return (dest);
+}
 
 /**
  * new_dog - creates a new dog
@@ -15,8 +45,8 @@ dog_t *new_dog(char *name, float age, char *owner)
 
 	if (newdog == NULL)
 		return (NULL);
-	newdog->name = name;
+	newdog->name = _strdup(name);
 	newdog->age = age;
-	newdog->owner = owner;
+	newdog->owner = _strdup(owner);
 	return (newdog);
 }
