9a92e308318b5df8faf8a237faa8a9c989615a6a
jashoney <4479@holbertonschool.com>
2022-03-07 12:52:52 +1100

    removed age fill task 4

--- 0x0E-structures_typedef/4-new_dog.c
+++ 0x0E-structures_typedef/4-new_dog.c
@@ -16,7 +16,7 @@ dog_t *new_dog(char *name, float age, char *owner)
 	if (newdog == NULL)
 		return (NULL);
 	newdog->name = name;
-	newdog->age = age;
+/*	newdog->age = age; */
 	newdog->owner = owner;
 	return (newdog);
 }
