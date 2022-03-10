322566b605e51859cfd81acf2e608dd42cf6e3ac
jashoney <4479@holbertonschool.com>
2022-03-10 13:24:07 +1100

    betty header file

--- 0x10-variadic_functions/variadic_functions.h
+++ 0x10-variadic_functions/variadic_functions.h
@@ -1,17 +1,7 @@
 #ifndef VARIADIC_FUNCTIONS_H
 #define VARIADIC_FUNCTIONS_H
 
-/**
- * struct fun  - gives a list of functions and their type
- * @use_type: the numerical operator to use
- * @f: the function related to that operator
-
-typedef struct fun
-{
-        char usetype;
-        void (*f)(va_list);
-} fun_t;
-*/
+int _putchar(char c);
 int sum_them_all(const unsigned int n, ...);
 void print_numbers(const char *separator, const unsigned int n, ...);
 void print_strings(const char *separator, const unsigned int n, ...);
