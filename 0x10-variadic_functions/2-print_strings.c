d232aa7b7c1b698e1f45e1ef7476376eb2bf764e
jashoney <4479@holbertonschool.com>
2022-03-10 13:06:40 +1100

    betty task 2 aagaaain

--- 0x10-variadic_functions/2-print_strings.c
+++ 0x10-variadic_functions/2-print_strings.c
@@ -25,7 +25,6 @@ void print_strings(const char *separator, const unsigned int n, ...)
 			printf("%s", current_string);
 		if (separator != NULL && (i < n - 1))
 			printf("%s", separator);
-		
 	}
 	va_end(my_strings);
 	putchar('\n');
