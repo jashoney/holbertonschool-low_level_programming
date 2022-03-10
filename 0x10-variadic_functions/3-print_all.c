fa530330dff1c367d15cfa43f8346c5b0c4cfe92
jashoney <4479@holbertonschool.com>
2022-03-10 18:05:06 +1100

    changed NULL test to exclude \n

--- 0x10-variadic_functions/3-print_all.c
+++ 0x10-variadic_functions/3-print_all.c
@@ -14,35 +14,36 @@ void print_all(const char * const format, ...)
 	unsigned int i = 0;
 	char *separator = "", *string;
 
-	if (format == NULL)
-		return;
-	va_start(args, format);
-	while (format[i] != '\0')
+	if (format != NULL)
 	{
-		switch (format[i])
+		va_start(args, format);
+		while (format[i] != '\0')
 		{
-			case 'c':
-				printf("%s%c", separator, va_arg(args, int));
-				separator = ", ";
-				break;
-			case 'i':
-				printf("%s%d", separator, va_arg(args, int));
-				separator = ", ";
-				break;
-			case 's':
-				string = va_arg(args, char *);
-				if (string == NULL || string[0] == '\0')
-					string = "(nil)";
-				printf("%s%s", separator, string);
-				separator = ", ";
-				break;
-			case 'f':
-				printf("%s%f", separator, va_arg(args, double));
-				separator = ", ";
-				break;
+			switch (format[i])
+			{
+				case 'c':
+					printf("%s%c", separator, va_arg(args, int));
+					separator = ", ";
+					break;
+				case 'i':
+					printf("%s%d", separator, va_arg(args, int));
+					separator = ", ";
+					break;
+				case 's':
+					string = va_arg(args, char *);
+					if (string == NULL || string[0] == '\0')
+						string = "(nil)";
+					printf("%s%s", separator, string);
+					separator = ", ";
+					break;
+				case 'f':
+					printf("%s%f", separator, va_arg(args, double));
+					separator = ", ";
+					break;
+			}
+			i++;
 		}
-		i++;
+		va_end(args);
 	}
-	va_end(args);
 	putchar('\n');
 }
