2b6ab10ae03f44cd1b208da2498011ace54f7f21
jashoney <4479@holbertonschool.com>
2022-03-17 12:51:05 +1100

    changed NULL test for head

--- 0x12-singly_linked_lists/4-free_list.c
+++ 0x12-singly_linked_lists/4-free_list.c
@@ -6,17 +6,17 @@ void free_list(list_t *head)
 {
 	list_t *current_node, *next_node;
 
-	if (head == NULL)
-		free(head);
-
-	current_node = head;
-	while (current_node->next != NULL)
+	if (head != NULL)
 	{
-		next_node = current_node->next;
+		current_node = head;
+		while (current_node->next != NULL)
+		{
+			next_node = current_node->next;
+			free(current_node->str);
+			free(current_node);
+			current_node = next_node;
+		}
 		free(current_node->str);
-		free(current_node);
-		current_node = next_node;
+		free(next_node);
 	}
-	free(current_node->str);
-	free(next_node);
 }
