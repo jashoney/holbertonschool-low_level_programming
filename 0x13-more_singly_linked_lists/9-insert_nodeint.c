03ed0aeb5a40a9608ae9dd8982c9eeee74db194c
jashoney <4479@holbertonschool.com>
2022-03-22 07:25:08 +1100

    betty task 9

--- 0x13-more_singly_linked_lists/9-insert_nodeint.c
+++ 0x13-more_singly_linked_lists/9-insert_nodeint.c
@@ -32,7 +32,7 @@ listint_t *insert_nodeint_at_index(listint_t **head, unsigned int idx, int v)
 	{
 		current = *head;
 		new_node->next = current->next;
-		*head = new_node; 
+		*head = new_node;
 		return (new_node);
 	}
 	current = *head;
