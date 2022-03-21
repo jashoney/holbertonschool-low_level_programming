1421f8b14d6db29805c9681458af71848586bf8f
jashoney <4479@holbertonschool.com>
2022-03-22 08:19:10 +1100

    reworked task 9 logic

--- 0x13-more_singly_linked_lists/9-insert_nodeint.c
+++ 0x13-more_singly_linked_lists/9-insert_nodeint.c
@@ -11,31 +11,26 @@
  * Return: NULL or  a ptr to the added node
  */
 
-listint_t *insert_nodeint_at_index(listint_t **head, unsigned int idx, int v)
+listint_t *insert_nodeint_at_index(listint_t **h, unsigned int idx, int v)
 {
 	unsigned int count = 0;
 	listint_t *current, *previous = NULL, *new_node;
 
-	if (head == NULL)
+	if (h == NULL)
 		return (NULL);
 	new_node = malloc(sizeof(*new_node));
 	if (new_node == NULL)
 		return (NULL);
 	new_node->n = v;
-	if (idx == 0 && *head == NULL)
-	{
-		*head = new_node;
-		new_node->next = NULL;
-		return (new_node);
-	}
+	if (*h == NULL && idx > 0)
+		return (NULL);
 	if (idx == 0)
 	{
-		current = *head;
-		new_node->next = current->next;
-		*head = new_node;
+		new_node->next = *h;
+		*h = new_node;
 		return (new_node);
 	}
-	current = *head;
+	current = *h;
 	while (current->next != NULL)
 	{
 		if (count == idx)
