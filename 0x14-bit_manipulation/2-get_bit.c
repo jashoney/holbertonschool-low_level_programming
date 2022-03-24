7fd5eb8fa9e811b403e6c8f1457cf85df94ed4af
jashoney <4479@holbertonschool.com>
2022-03-24 16:30:05 +1100

    bitwise task 2

--- 0x14-bit_manipulation/2-get_bit.c
+++ 0x14-bit_manipulation/2-get_bit.c
@@ -1,42 +1,7 @@
-#include <stdio.h>
-#include <unistd.h>
 #include "main.h"
-/**
- * power_of_2 - calcs the power of 2 to size
- * @size: the exponent
- * Return: the calc
- */
-
-unsigned long power_of_2(int size)
-{
-	unsigned long power;
-	int i = 1;
-
-	if (size < 1)
-		return (1);
-	power = 1;
-	while (i <= size)
-	{
-		i++;
-		power = power * 2;
-	}
-	return (power);
-}
 
 /**
- * _putchar - writes the character c to stdout
- * @c: The character to print
- *
- * Return: On success 1.
- * On error, -1 is returned, and errno is set appropriately.
- */
-int _putchar(char c)
-{
-	return (write(1, &c, 1));
-}
-
-/**
- * get_bit - takes an integer and converts it to a binary string
+ * get_bit - finds the value of a bit at index in a binary number 
  * @n: the number
  * @index: the index of the string to return
  * Return: the bit at index or -1 if fail
@@ -44,39 +9,19 @@ int _putchar(char c)
 
 int get_bit(unsigned long int n, unsigned int index)
 {
-	unsigned long int i = 1, power2 = 2;
-	char bit;
+	unsigned long int i = 0;
 
-	if (n > 1)
+	while (i < 32)
 	{
-		while (n >= power2)
+		if (i == index)
 		{
-			i++;
-			power2 = power2 * 2;
+			if (n & 1)
+				return (1);
+			else	
+				return (0);
 		}
-		i--;
-
-		if (index > i + 1)
-			return (-1);
-
-		power2 = power_of_2(i);
-		while (i > 0)
-		{
-			if (n >= power2)
-			{
-				n = n - power2;
-				bit = 1;
-			}
-			else
-				bit = 0;
-			if (index == i)
-				return (bit);
-			i--;
-			power2 = power_of_2(i);
-		}
+		n = n >> 1;
+		i++;
 	}
-	if (index == 0)
-		return (n);
-	else
-		return (-1);
+	return (-1);
 }
