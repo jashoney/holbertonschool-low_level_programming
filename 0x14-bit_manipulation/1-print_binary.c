f07a9e4422011f5331948b5d7fefe023003b91d0
jashoney <4479@holbertonschool.com>
2022-03-24 17:27:58 +1100

    changed task 1 to recursion bitwise

--- 0x14-bit_manipulation/1-print_binary.c
+++ 0x14-bit_manipulation/1-print_binary.c
@@ -1,65 +1,19 @@
-#include <unistd.h>
+#include <stdlib.h>
 #include "main.h"
-/**
- * power_of_2 - calcs the power of 2 to size
- * @size: the exponent
- * Return: the calc
- */
-
-unsigned long power_of_2(int size)
-{
-	unsigned long power;
-	int i = 1;
-
-	if (size < 1)
-		return (1);
-	power = 1;
-	while (i <= size)
-	{
-		i++;
-		power = power * 2;
-	}
-	return (power);
-}
 
 /**
- * print_binary - takes an integer and converts it to a binary string
- * @n: the number
- * Return: the binary string representing the number
+ * print_binary - recursively splits out each binary bit of an integer
+ * @n: the integer
+ * Return: the binary form of the number n
  */
 
 void print_binary(unsigned long int n)
 {
-	unsigned long int i = 1, power2 = 2;
-
-	if (n > 2)
+	if (n > 1)
 	{
-		while (n >= power2)
-		{
-			i++;
-			power2 = power2 * 2;
-		}
-		i--;
+		print_binary(n >> 1);
 	}
-	power2 = power_of_2(i);
-	if (n > 2)
-	{
-		while (i > 0)
-		{
-			if (n >= power2)
-			{
-				_putchar('1');
-				n = n - power2;
-			}
-			else
-				_putchar('0');
-			i--;
-			power2 = power_of_2(i);
-		}
-	}
-
-	if (n == 0)
-		_putchar('0');
-	else
-		_putchar('1');
+	_putchar((n & 1) + '0');
 }
+
+
