9730a452b4959339d72ee91f431e4f6c232cd832
jashoney <4479@holbertonschool.com>
2022-03-28 16:53:18 +1100

    changed task 2 logic

--- 0x15-file_io/2-append_text_to_file.c
+++ 0x15-file_io/2-append_text_to_file.c
@@ -27,14 +27,24 @@ int append_text_to_file(const char *filename, char *text_content)
 
 	fd = open(filename, O_APPEND | O_WRONLY);
 	if (fd == -1)
-		fd = open(filename, O_CREAT | O_RDWR, S_IRUSR | S_IWUSR);
+		return (-1);
+
+	wf = access(filename, W_OK);
+	if (wf == -1)
+	{
+		close(fd);
+		return (-1);
+	}
 
 	while (text_content[i] != '\0')
 		i++;
 
 	wf = write(fd, text_content, i);
 	if (wf == -1)
+	{
+		close(fd);
 		return (-1);
+	}
 	close(fd);
 	return (1);
 }
