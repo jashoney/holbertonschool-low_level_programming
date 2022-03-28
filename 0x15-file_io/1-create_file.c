5dc2950f1e95d3d9d8891d1657706c61ea2ce4cb
jashoney <4479@holbertonschool.com>
2022-03-28 16:38:55 +1100

    changed task 1 logic

--- 0x15-file_io/1-create_file.c
+++ 0x15-file_io/1-create_file.c
@@ -25,16 +25,9 @@ int create_file(const char *filename, char *text_content)
 	while (text_content[i] != '\0')
 		i++;
 
-	fd = open(filename, O_TRUNC | O_WRONLY);
+	fd = open(filename, O_CREAT | O_TRUNC | O_WRONLY, S_IRUSR | S_IWUSR);
 	if (fd == -1)
-	{
-		fd = open(filename, O_CREAT | O_WRONLY, S_IRUSR | S_IWUSR);
-		if (fd == -1)
-			return (-1);
-	}
-
-	while (text_content[i] != '\0')
-		i++;
+		return (-1);
 
 	wf = write(fd, text_content, i);
 	close(fd);
