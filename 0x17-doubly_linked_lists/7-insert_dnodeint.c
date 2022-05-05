3e0211c6160c0cf1ebe108d810e2ead8d0305f09
jashoney <4479@holbertonschool.com>
2022-05-05 14:28:59 +1000

    rewrote task 7

--- 0x17-doubly_linked_lists/7-insert_dnodeint.c
+++ 0x17-doubly_linked_lists/7-insert_dnodeint.c
@@ -1,6 +1,24 @@
 #include <stdio.h>
 #include <stdlib.h>
 #include "lists.h"
+/**
+ * create_new_node - does what it says
+ * @n: value in the node
+ * Return: a ptr to the node or NULL
+ */
+
+dlistint_t *create_new_node(int n)
+{
+	dlistint_t *new;
+
+	new = malloc(sizeof(*new));
+	if (new == NULL)
+		return (NULL);
+	new->n = n;
+	new->prev = NULL;
+	new->next = NULL;
+	return (new);
+}
 
 /**
  * insert_dnodeint_at_index - inserts a node at index
@@ -13,38 +31,42 @@
 dlistint_t *insert_dnodeint_at_index(dlistint_t **h, unsigned int idx, int n)
 {
 	unsigned int count = 0;
-	dlistint_t *new, *temp, *prev = NULL;
+	dlistint_t *new = NULL, *temp = NULL, *prev = NULL;
 
 	if (h == NULL)
 		return (NULL);
-
-	new = malloc(sizeof(*new));
+	if (idx == 0)
+	{
+		new = add_dnodeint(h, n);
+		return (new);
+	}
+	temp = *h;
+	new = create_new_node(n);
 	if (new == NULL)
 		return (NULL);
-	new->n = n;
-	new->prev = NULL;
-	new->next = NULL;
-
-	temp = *h;
 	while (temp != NULL && count < idx)
 	{
 		prev = temp;
 		temp = temp->next;
 		count++;
 	}
-
-	if (count < idx - 1)
+	if (count + 1 < idx)
 		return (NULL);
-	else if (count == idx - 1)
+	if (count + 1 == idx)
+	{
 		temp->next = new;
+		new->prev = temp;
+	}
 	else
 	{
+		new->next = temp;
 		if (prev != NULL)
 		{
 			new->prev = prev;
 			prev->next = new;
 		}
-		new->next = temp;
+		else
+			temp->prev = new;
 	}
 	return (new);
 }
