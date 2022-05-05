a1756e24ab7a0b1e655f081adaacec6257cf44ee
jashoney <4479@holbertonschool.com>
2022-05-05 14:51:25 +1000

    moved list_len test

--- 0x17-doubly_linked_lists/7-insert_dnodeint.c
+++ 0x17-doubly_linked_lists/7-insert_dnodeint.c
@@ -3,6 +3,27 @@
 #include "lists.h"
 
 /**
+ * list_len - returns the number of nodes in a list
+ * @h: ptr to the head node
+ * Return: the number of nodes
+ */
+
+int list_len(const dlistint_t *h)
+{
+	int count = 0;
+
+	if (h == NULL)
+		return (count);
+
+	while (h != NULL)
+	{
+		h = h->next;
+		count++;
+	}
+	return (count);
+}
+
+/**
  * insert_dnodeint_at_index - inserts a node at index
  * @h: a ptr to a ptr to the head node
  * @idx: the index of the required node
@@ -12,7 +33,7 @@
 
 dlistint_t *insert_dnodeint_at_index(dlistint_t **h, unsigned int idx, int n)
 {
-	unsigned int count = 0;
+	unsigned int count = 0, check = 0;
 	dlistint_t *new = NULL, *temp = NULL, *prev = NULL;
 
 	if (h == NULL)
@@ -26,14 +47,15 @@ dlistint_t *insert_dnodeint_at_index(dlistint_t **h, unsigned int idx, int n)
 	new->next = NULL;
 	if (new == NULL)
 		return (NULL);
+	check = list_len(*h);
+	if (check < idx)
+		return (NULL);
 	while (temp != NULL && count < idx)
 	{
 		prev = temp;
 		temp = temp->next;
 		count++;
 	}
-	if (count + 1 < idx)
-		return (NULL);
 	if (count + 1 == idx)
 	{
 		temp->next = new;
