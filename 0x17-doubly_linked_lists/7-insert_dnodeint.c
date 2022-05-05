790f28045ea3c2d8a1a6fb8a89be9468fc7b9c40
jashoney <4479@holbertonschool.com>
2022-05-05 13:32:04 +1000

    changed return value

--- 0x17-doubly_linked_lists/7-insert_dnodeint.c
+++ 0x17-doubly_linked_lists/7-insert_dnodeint.c
@@ -41,7 +41,7 @@ dlistint_t *insert_dnodeint_at_index(dlistint_t **h, unsigned int idx, int n)
 			prev->next = new;
 		}
 		new->next = temp;
-		return (temp);
+		return (new);
 	}
 	return (NULL);
 }
