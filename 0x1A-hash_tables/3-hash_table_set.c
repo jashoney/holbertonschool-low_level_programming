9bfead41c6336d26dcc5e04554af3cd9a7a32995
jashoney <4479@holbertonschool.com>
2022-05-27 11:41:08 +1000

    added freeing of strings

--- 0x1A-hash_tables/3-hash_table_set.c
+++ 0x1A-hash_tables/3-hash_table_set.c
@@ -12,7 +12,7 @@ int hash_table_set(hash_table_t *ht, const char *key, const char *value)
 {
 	unsigned long int size = ht->size, index;
 	hash_node_t *new_node = NULL, *current = NULL;
-	char *key_copy;
+	char *key_copy, *value_copy;
 
 	if (ht == NULL || key == NULL)
 		return (0);
@@ -21,6 +21,7 @@ int hash_table_set(hash_table_t *ht, const char *key, const char *value)
 		return (0);
 
 	key_copy = strdup(key);
+	value_copy = strdup(value);
 	index = key_index((const unsigned char *)key, size);
 
 	current = ht->array[index];
@@ -30,7 +31,7 @@ int hash_table_set(hash_table_t *ht, const char *key, const char *value)
 		{
 			free(key_copy);
 			free(current->value);
-			current->value = strdup(value);
+			current->value = value_copy;
 			return (1);
 		}
 		current = current->next;
@@ -38,9 +39,13 @@ int hash_table_set(hash_table_t *ht, const char *key, const char *value)
 
 	new_node = malloc(sizeof(*new_node));
 	if (new_node == NULL)
+	{
+		free(key_copy);
+		free(value_copy);
 		return (0);
+	}
 	new_node->key = key_copy;
-	new_node->value = strdup(value);
+	new_node->value = value_copy;
 	new_node->next = ht->array[index];
 	ht->array[index] = new_node;
 
