24d0aa60e2835bd04e85b827c866bff8e249a42b
jashoney <4479@holbertonschool.com>
2022-05-27 11:37:12 +1000

    added freeing of strings

--- 0x1A-hash_tables/3-hash_table_set.c
+++ 0x1A-hash_tables/3-hash_table_set.c
@@ -28,19 +28,20 @@ int hash_table_set(hash_table_t *ht, const char *key, const char *value)
 	{
 		if (strcmp(key_copy, current->key) == 0)
 		{
+			free(key_copy);
+			free(current->value);
 			current->value = strdup(value);
 			return (1);
 		}
 		current = current->next;
 	}
 
-	current = ht->array[index];
 	new_node = malloc(sizeof(*new_node));
 	if (new_node == NULL)
 		return (0);
-	new_node->key = strdup(key);
+	new_node->key = key_copy;
 	new_node->value = strdup(value);
-	new_node->next = current;
+	new_node->next = ht->array[index];
 	ht->array[index] = new_node;
 
 	return (1);
